5d26637af39da75fe4991c9ae4c0e5e7856aebcc
Marco <marcoduarte92sm@gmail.com>
2024-03-06 21:12:30 -0300

    add Principal

--- src/util/Principal.java
+++ src/util/Principal.java
@@ -1,5 +1,6 @@
 package util;
 
+import graphs.MatrizAdjacencia;
 import graphs.Vertice;
 
 import java.util.ArrayList;
@@ -21,6 +22,17 @@ public class Principal {
                         new Vertice("D")
                 )
         );
+
+        MatrizAdjacencia matriz = new MatrizAdjacencia(vertices);
+        matriz.conectarVertices(0, 1);
+        matriz.conectarVertices(0, 2);
+        matriz.conectarVertices(1, 2);
+        matriz.conectarVertices(1, 3);
+
+        System.out.println("Grau A: " + vertices.get(0).getGrau());
+        System.out.println("Grau B: " + vertices.get(1).getGrau());
+        System.out.println("Grau C: " + vertices.get(2).getGrau());
+        System.out.println("Grau D: " + vertices.get(3).getGrau());
 
 
     }
